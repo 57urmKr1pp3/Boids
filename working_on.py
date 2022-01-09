8e9b795a6a3d97a123f2dc997d44646ae1732a35
57urmKr1pp3 <oliver.fritzler@gmx.de>
2022-01-09 21:06:14 +0100

    Berechnung hinzugefügt Rotation noch fehlerhaft

--- working_on.py
+++ working_on.py
@@ -1,21 +1,8 @@
 import math
-from random import randint
 
-from Main import Liste_Boids
-#############################
-#in Main.py
-#############################
-def input(key):
-    if held_keys["+"]:
-        temp = Boid(i, randint(-10, 10), randint(-10, 10), randint(-10, 10), randint(-20, 20), randint(-20, 20), randint(-20, 20))
-        Liste_Boids.append(temp)
-    if held_keys["-"]:
-        Liste_Boids[len(Liste_Boids)-1].disable()
-        Liste_Boids.pop()
-
-#############################
-#in Boids und update()
-#############################
+# #############################
+# #in Boids und update()
+# #############################
 def rotation(self):
     #Rotation davor
     tempRotX = self.rotation_x
@@ -44,12 +31,43 @@ def rotation(self):
     #Berechnung
     #bei der Berechnung benutze ich 2D-Vektoren um die einzelnen Winkel zu berechnen
     EinheitsvektorXY = [1, 0]
-    EinheitsvektorYXZ = [0, 1]
-    EinheitsvektorZY = [1, 0]
+    EinheitsvektorXZ = [0, 1]
+    EinheitsvektorYZ = [1, 0]
     VektorXY = [self.directionX, self.directionY]
-    VektorZY = [self.directionZ, self.directionY]
+    VektorYZ = [self.directionZ, self.directionY]
     VektorXZ = [self.directionX, self.directionX]
-    angleXY
-    angleZX
-    angleYZ
+    # VektorXY = [2, 5]
+    # VektorYZ = [6, 3]
+    # VektorXZ = [4, 1]
+    #Berechnung von Skalarprodukt und Betragprdukt, da sonst falsches Ergebnis
+    #Winkel zwischen XY
+    skalarXY = VektorXY[0]*EinheitsvektorXY[0] + VektorXY[1]*EinheitsvektorXY[1]
+    betragXY = math.sqrt(VektorXY[0]**2+VektorXY[1]**2)*math.sqrt(EinheitsvektorXY[0]**2+EinheitsvektorXY[1]**2)
+    radXY = math.acos(skalarXY/betragXY)
+    angleXY = math.degrees(radXY)
+    #Winkel zwischen XZ
+    skalarXZ = VektorXZ[0]*EinheitsvektorXZ[0] + VektorXZ[1]*EinheitsvektorXZ[1]
+    betragXZ = math.sqrt(VektorXZ[0]**2+VektorXZ[1]**2)*math.sqrt(EinheitsvektorXZ[0]**2+EinheitsvektorXZ[1]**2)
+    radXZ = math.acos(skalarXZ/betragXZ)
+    angleXZ = math.degrees(radXZ)
+    #Winkel zwischen YZ
+    skalarYZ = VektorYZ[0]*EinheitsvektorYZ[0] + VektorYZ[1]*EinheitsvektorYZ[1]
+    betragYZ = math.sqrt(VektorYZ[0]**2+VektorYZ[1]**2)*math.sqrt(EinheitsvektorYZ[0]**2+EinheitsvektorYZ[1]**2)
+    radYZ = math.acos(skalarYZ/betragYZ)
+    angleYZ = math.degrees(radYZ)
     #Rotieren
+    if tempPosX > tempPosY and tempPosX > tempPosZ:
+        #z-Rotation
+        self.rotation_z = -angleXY
+        #x-Rotation
+        self.rotation_x = -angleYZ
+    if tempPosY > tempPosX and tempPosY > tempPosZ:
+        #x-Rotation
+        self.rotation_x = -angleYZ
+        #y-Rotation
+        self.rotation_y = angleXZ
+    if tempPosZ > tempPosX and tempPosZ > tempPosY:
+        #x-Rotation
+        self.rotation_x = -angleYZ
+        #z-Rotation
+        self.rotation_z = -angleXY
