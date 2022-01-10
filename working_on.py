6c0c32a95d53316c951c8aa4b3983f857a71aabb
57urmKr1pp3 <oliver.fritzler@gmx.de>
2022-01-10 17:06:51 +0100

    Winkelberechnung angepasst

--- working_on.py
+++ working_on.py
@@ -31,7 +31,7 @@ def rotation(self):
     #Berechnung
     #bei der Berechnung benutze ich 2D-Vektoren um die einzelnen Winkel zu berechnen
     EinheitsvektorXY = [1, 0]
-    EinheitsvektorXZ = [0, 1]
+    EinheitsvektorXZ = [1, 0]
     EinheitsvektorYZ = [1, 0]
     VektorXY = [self.directionX, self.directionY]
     VektorYZ = [self.directionZ, self.directionY]
@@ -45,50 +45,61 @@ def rotation(self):
         skalarXY = VektorXY[0]*EinheitsvektorXY[0] + VektorXY[1]*EinheitsvektorXY[1]
         betragXY = math.sqrt(VektorXY[0]**2+VektorXY[1]**2)*math.sqrt(EinheitsvektorXY[0]**2+EinheitsvektorXY[1]**2)
         radXY = math.acos(skalarXY/betragXY)
-        angleXY = math.degrees(radXY)
+        if VektorXY[1] < 0:
+            angleXY = -(-180+math.degrees(radXY))
+        else:
+            angleXY = -math.degrees(radXY)
         #Winkel zwischen XZ
         skalarXZ = VektorXZ[0]*EinheitsvektorXZ[0] + VektorXZ[1]*EinheitsvektorXZ[1]
         betragXZ = math.sqrt(VektorXZ[0]**2+VektorXZ[1]**2)*math.sqrt(EinheitsvektorXZ[0]**2+EinheitsvektorXZ[1]**2)
         radXZ = math.acos(skalarXZ/betragXZ)
-        angleXZ = math.degrees(radXZ)
+        if VektorXZ[1] < 0:
+            angleXZ = -(-180+math.degrees(radXZ))
+        else:
+            angleXZ = -math.degrees(radXZ)
+
         #Winkel zwischen YZ
         skalarYZ = VektorYZ[0]*EinheitsvektorYZ[0] + VektorYZ[1]*EinheitsvektorYZ[1]
         betragYZ = math.sqrt(VektorYZ[0]**2+VektorYZ[1]**2)*math.sqrt(EinheitsvektorYZ[0]**2+EinheitsvektorYZ[1]**2)
         radYZ = math.acos(skalarYZ/betragYZ)
-        angleYZ = math.degrees(radYZ)
+        if VektorYZ[1] < 0:
+            angleYZ = -(-180+math.degrees(radYZ))
+        else:
+            angleYZ = -math.degrees(radYZ)
+            
     except:
         pass
     #Rotieren
     if tempPosX > tempPosY and tempPosX > tempPosZ:
         #z-Rotation
         if angleXY < 0:
-            self.rotation_z = -angleXY
+            self.world_rotation_z = angleXY
         else:
-            self.rotation_z = angleXY
+            self.world_rotation_z = angleXY
         #x-Rotation
         if angleXY < 0:
-            self.rotation_x = -angleYZ
+            self.world_rotation_x = angleYZ
         else:
-            self.rotation_x = angleYZ
+            self.world_rotation_x = angleYZ
     if tempPosY > tempPosX and tempPosY > tempPosZ:
         #x-Rotation
         if angleXY < 0:
-            self.rotation_x = -angleYZ
+            self.world_rotation_x = angleYZ
         else:
-            self.rotation_x = angleYZ
+            self.world_rotation_x = angleYZ
         #y-Rotation
         if angleXZ < 0:
-            self.rotation_y = -angleXZ
+            self.world_rotation_y = angleXZ
         else:
-            self.rotation_y = angleXZ
+            self.world_rotation_y = angleXZ
     if tempPosZ > tempPosX and tempPosZ > tempPosY:
         #x-Rotation
         if angleXY < 0:
-            self.rotation_x = -angleYZ
+            self.world_rotation_x = angleYZ
         else:
-            self.rotation_x = angleYZ
+            self.world_rotation_x = angleYZ
         #z-Rotation
         if angleXY < 0:
-            self.rotation_z = -angleXY
+            self.world_rotation_z = angleXY
         else:
-            self.rotation_z = angleXY
+            self.world_rotation_z = angleXY
