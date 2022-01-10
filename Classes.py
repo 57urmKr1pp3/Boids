bbd3c4330271e6f6b9727c866cc944f1046ab145
57urmKr1pp3 <oliver.fritzler@gmx.de>
2022-01-10 17:03:32 +0100

    Modus hinzugefügt

--- Classes.py
+++ Classes.py
@@ -5,7 +5,7 @@ class Boid (Entity):
     #Konstruktor der Klasse Entity und Boid wird aufgerufen
     #Position ist ein 3 dimensioanler Vektor mit x, y und z Koordinate
     #Direction ist die Geschwindigkeit mit welcher sich der Boid bewegt, welcher ebenfalls ein 3 dimensioanler Vektor ist
-    def __init__(self, posX, posY, posZ, dirX, dirY, dirZ):
+    def __init__(self, posX, posY, posZ, dirX, dirY, dirZ, mode):
         super().__init__()
         self.position = (posX, posY, posZ)
         self.directionX = dirX
@@ -15,6 +15,7 @@ class Boid (Entity):
         self.model = 'sprites'
         self.scale = 5
         self.collider = "boxes"
+        self.mode = mode
         #self.rotation = (self.directionX, self.directionY, self.directionZ)
         
     #Getter geben die Werte der Eigenschaft zurück
@@ -22,8 +23,10 @@ class Boid (Entity):
         return self.position
 
     def getDirection(self):
-        return self.direction
+        return (self.directionX, self.directionY, self.directionZ)
 
+    def getMode(self):
+        return self.mode
     #Setter berschreiben die Eigenschaften mit den neuen Werten
     def setPosition(self, nPosX, nPosY, nPosZ):
         self.position =(nPosX, nPosY, nPosZ)
@@ -37,6 +40,8 @@ class Boid (Entity):
     def setDirectionZ(self, nDirZ):
         self.directionZ = nDirZ
 
+    def setMode(self, nMode):
+        self.mode = nMode
     #Update-Funktion bewegt den Boid durch Vektoraddition der Position und der Richtung
     #damit die bewegung gleichmäßig unabhängig von den aeusseren Umstaenden passiert wird mit time.dt multipliziert
     def update(self):
@@ -44,32 +49,34 @@ class Boid (Entity):
         self.x += self.directionX * time.dt
         self.y += self.directionY * time.dt
         self.z += self.directionZ * time.dt
-        #Richtungsänderung beim Erreichen der Grenze
-        if self.x <= -49:
-            self.directionX = -self.directionX
-        elif self.x >= 49:
-            self.directionX = -self.directionX
-        if self.y <= -49:
-            self.directionY = -self.directionY
-        elif self.y >= 49:
-            self.directionY = -self.directionY
-        if self.z <= -49:
-            self.directionZ = -self.directionZ
-        elif self.z >= 49:
-            self.directionZ = -self.directionZ
-        #Teleport zur anderen Seite
-        # if self.x <= -50:
-        #     self.x = 49
-        # if self.x >= 50:
-        #     self.x = -49
-        # if self.y <= -50:
-        #     self.y = 49
-        # if self.y >= 50:
-        #     self.y = -49
-        # if self.z <= -50:
-        #     self.z = 49
-        # if self.z >= 50:
-        #     self.z = -49
+        if self.mode == 1:
+            #Richtungsänderung beim Erreichen der Grenze
+            if self.x <= -49:
+                self.directionX = -self.directionX
+            elif self.x >= 49:
+                self.directionX = -self.directionX
+            if self.y <= -49:
+                self.directionY = -self.directionY
+            elif self.y >= 49:
+                self.directionY = -self.directionY
+            if self.z <= -49:
+                self.directionZ = -self.directionZ
+            elif self.z >= 49:
+                self.directionZ = -self.directionZ
+        elif self.mode == 2:
+            #Teleport zur anderen Seite
+            if self.x <= -50:
+                self.x = 49
+            if self.x >= 50:
+                self.x = -49
+            if self.y <= -50:
+                self.y = 49
+            if self.y >= 50:
+                self.y = -49
+            if self.z <= -50:
+                self.z = 49
+            if self.z >= 50:
+                self.z = -49
         #Bug beim hinzufügen der Boids verursacht das Boids ausserhalb des Würfels sind
         if self.x < -50:
             self.x = -49
