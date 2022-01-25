f6998a4835debe192f63041f8b8fce507818c9e1
57urmKr1pp3 <oliver.fritzler@gmx.de>
2022-01-25 21:05:11 +0100

    zusammenführung der Eigenschaften in eine Funktion

---
+++ Versuch 2/working_on.py
@@ -0,0 +1,30 @@
+def eigenschaften(self):
+    #proximity
+    proximity = []
+    for i in Liste_Boids:
+        if i != self:
+            distanceBoids = distance(self.position, i.position)
+            if distanceBoids < 5:
+                proximity.append(i)
+    #alignment
+    Boid1XR = self.rotation_x
+    Boid1YR = self.rotation_y
+    Boid1ZR = self.rotation_z
+    for i in proximity:
+        Boid2XR = i.rotation_x
+        Boid2YR = i.rotation_y
+        Boid2ZR = i.rotation_z
+        avgXR = (Boid1XR + Boid2XR)/2
+        avgYR = (Boid1YR + Boid2YR)/2
+        avgZR = (Boid1ZR + Boid2ZR)/2
+        self.rotation_x = avgXR
+        self.rotation_y = avgYR
+        self.rotation_z = avgZR
+
+    #seperation
+    HitInfo = raycast(position = self.position, direction = self.up, distance = 5, traverse_target = scene, ignore = ())
+    if HitInfo.hit:
+        self.rotation = self.right * self.vel
+
+    #cohesion    
+    
