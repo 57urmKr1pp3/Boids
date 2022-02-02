0f8bdf908d0689156595d67e687b1522a7d04cc9
57urmKr1pp3 <oliver.fritzler@gmx.de>
2022-02-02 22:40:27 +0100

    Auskommentiert

--- Versuch 2/Main.py
+++ Versuch 2/Main.py
@@ -16,7 +16,8 @@ class Boid (Entity):
         self.mode = mode
         self.collider = 'box'
 
-    #Getter
+    #Getter 
+    #geben die Werte der Eigenschaften zurück
     def getModel(self):
         return self.model
 
@@ -47,7 +48,8 @@ class Boid (Entity):
     def getCollider(self):
         return self.collider
     
-    #Setter
+    #Setter 
+    #übeschreiben die Eigenschaften mit neuen Werten
     def setPosition(self, posX, posY, posZ):
         self.position = (posX, posY, posZ)
     
@@ -77,33 +79,43 @@ class Boid (Entity):
 
     #Bewegung
     def updateVel(self):
+        #Die Geschwindigkeit wird durch Addition der Beschleunigung so lange erhöht bis sie das Maximum erreicht hat
         if self.vel < self.maxVel:
             self.vel += self.acel
 
     def closeBoids(self):
+        #Diese Funktion ist dazu da um die Boids in der Nähe zu erfassen
         close = []
+        #Dafür wird die Liste benutzt, die bei der Erstellung der Boids befüllt wird
+        #jeder Boid außer man selbst wird in dieser for-Schleife durchgegangen
         for i in Liste_Boids:
             if i != self:
+                #wenn die Distanz zwischen dem self.Boid und dem zu vergleichenden Boid unter 5 liegt wird der vergleichende Boid in die Liste mit den Boids in der Nähe hinzugefügt
                 distanceBoids = distance(self.position, i.position)
                 if distanceBoids < 5:
                     close.append(i)
         return close
 
     def alignment(self):
+        #Die Boids in der Nähe werden zwischengespeichert
         proximity = self.closeBoids()
+        #Die einzelnen Rotationswerte und die Geschwindigkeit des self.Boids werden hier gespeichert
         Boid1XR = self.rotation_x
         Boid1YR = self.rotation_y
         Boid1ZR = self.rotation_z
         Boid1V = self.vel
-        for i in proximity:
+        for i in proximity: # für jeden Boid in der Nähe wird das Alignment-Verfahren durchgegangen
+            #Rotations und Geschwindikeitswerte des zu vergleichenden Boids zwischengespeichert
             Boid2XR = i.rotation_x
             Boid2YR = i.rotation_y
             Boid2ZR = i.rotation_z
             Boid2V = i.vel
+            #Die Durchschnitte der einzelnen Werte werden hier berechnet
             avgXR = (Boid1XR + Boid2XR)/2
             avgYR = (Boid1YR + Boid2YR)/2
             avgZR = (Boid1ZR + Boid2ZR)/2
             avgV = (Boid1V + Boid2V)/2
+            #Die Durchschnittswerte überschreiben die Rotations und Geschwindigkeitswerte des self.Boid
             self.rotation_x = avgXR
             self.rotation_y = avgYR
             self.rotation_z = avgZR
@@ -114,7 +126,6 @@ class Boid (Entity):
         # for i in proximity:
         #     if distance(self.position, i.position) < 2:
         #         self.position -= Vec3(i.position + self.position)
-        proximity = self.closeBoids()
         #Versuch 1
         #ProxPos = []
         #for i in proximity:
@@ -127,14 +138,17 @@ class Boid (Entity):
         #    pass
 
         #Versuch 2
+        #Die Boids in der Nähe werden zwischengespeichert
+        proximity = self.closeBoids()
         if len(proximity) > 1:
             for i in proximity:
+                #wenn die Distanz zwischen dem self.Boid und des zu vergeichenden Boids <2 ist wird die Mitte zwischen den beiden Boids berechnet
                 if distance(self.position, i.position) < 2:
                     center = (self.position + i.position)/2
+                    #Die Differenz zwischen der Mitte und der eigenen Position wird von der Position subtrahiert um den Abstand zu erhöhen
                     self.position -= Vec3(center - self.position) * .005
 
     def cohesion(self):
-        proximity = self.closeBoids()
         #Versuch 1
         #ProxPos = []
         #for i in proximity:
@@ -146,30 +160,38 @@ class Boid (Entity):
         #except:
         #    pass
         #Versuch 2
+        #Die Boids in der Nähe werden gespeichert
+        proximity = self.closeBoids()
+        #Die Mitte aller Boids in der Nähe wird berechnet
         center = self.position
         if len(proximity) > 1:
             for i in proximity:
                 center = (center + i.position)/2
+        #falls die Distanz des Boids zu der Mitte groß genug ist wird der Abstand zur Mitte der Position hinzugefügt
+        #damit die Boids immer in die Mitte des Flocks ziehen
         if distance(self.position, center) >= 2:
-            self.position += Vec3(center - self.position) * 0.005
+            self.position += Vec3(center - self.position) * .005
 
 
 
     def avoidWall(self):
         #Raycasts
+        #Ein Raycast wird erzeugt der in die Bewegungsrichtung 5 Einheiten weit ausgerchtet ist 
         raycastup = raycast(origin = self.position, direction = self.up, distance = 5, traverse_target = scene, ignore = (self,), debug = True)
+        #Wenn dieser Raycast etwas berührt werden weitere Raycast erstellt, die in 4 weitere Richtungen ausgerichtet sind
         if raycastup.hit:
             raycastfront = raycast(origin = self.position, direction = (self.up + self.forward), distance = 10, traverse_target = scene, ignore = (self,), debug = True)
             raycastback = raycast(origin = self.position, direction = (self.up + self.back), distance = 10, traverse_target = scene, ignore = (self,), debug = True)
             raycastleft = raycast(origin = self.position, direction = (self.up + self.left), distance = 10, traverse_target = scene, ignore = (self,), debug = True)
             raycastright = raycast(origin = self.position, direction = (self.up + self.right), distance = 10, traverse_target = scene, ignore = (self,), debug = True)
-
+            #Die Distanz zu dem getroffenen Objektes von jedem Raycast wird gespeichert
             distance_up = raycastup.distance
             distance_front = raycastfront.distance
             distance_back = raycastback.distance
             distance_left = raycastleft.distance
             distance_right = raycastright.distance
-
+            
+            #je nach kleinster Distanz werden verschiedene Rotationsänderung durchgeführt
             #up kleinste Distanz
             if (distance_up <= distance_front) and (distance_up <= distance_back) and (distance_up <= distance_left) and (distance_up <= distance_right):
                 self.rotation += self.back * self.vel
@@ -191,10 +213,14 @@ class Boid (Entity):
                 self.rotation += self.left * self.vel
             
     def move(self):
+        #Die Posiiton wird durch Multiplikation der Geschwindigkeit und der Bewegungsrichtung verändert
+        #Die Bewegungsrichtung ist hier up/hoch, da die Modeldatei danach ausgerichtet ist
         self.position += self.up * (self.vel/1000)
         self.updateVel()
         #Warp Modus
+
         if self.mode == 1:
+            #falls die Position des Boids nach der Bewegung die der Wände überschreiten wird die Position auf die gegenüberliegende Seite gesetzt
             if self.x <= -49:
                 self.x = 48
             if self.x >= 49:
@@ -210,10 +236,14 @@ class Boid (Entity):
 
         #Wändevermeidung
         elif self.mode == 2:
+            #Funktion zur Wändevermeidung wird aufgerufen
             self.avoidWall()
 
         #Pong-Modus
         elif self.mode == 3:
+            #bei Überschreitung des Bereichs soll die Rotation verändert werden
+            #Problem: Rotationsordnung ist zu spezifisch sodass man es nicht allgemein verfassen kann
+            #         ebenfalls funktioniert die Rechnung: Rotation*-1-180 nicht wegen ^
             if self.x <= -49:
                 self.rotation_y = -self.rotation_y
             if self.x >= 49:
@@ -228,14 +258,17 @@ class Boid (Entity):
                 self.rotation_z = -self.rotation_z
 
     def update(self):
+        #Funktion zur Bewegung wird aufgerufen
         self.move()
+        #Funktion zur Einhaltung der Regeln werden aufgerufen
         self.alignment()
-        #self.seperation()
-        #self.cohesion()
+        self.seperation()
+        self.cohesion()
 
-def create_wireframe():
+def createWireframe():
     #05.01.2022
     #Wireframe
+    #Erzeugung des Würfels
     wf1 = Entity(model = "cube", collider = 'box', position = (0, -51, -51), scale_x = 102)
     wf2 = Entity(model = "cube", collider = 'box', position = (-51, -51, 0), scale_z = 102)
     wf3 = Entity(model = "cube", collider = 'box', position = (0, -51, 51), scale_x = 102)
@@ -255,7 +288,8 @@ def create_wireframe():
     w5 = Entity(model = 'cube', collider = 'box', position = (0,-52,0), scale = (101,0,101), color = color.red, alpha = 0)
     w5 = Entity(model = 'cube', collider = 'box', position = (0,52,0), scale = (101,0,101), color = color.red, alpha = 0)
 
-def create_boids(anzahl):
+def createBoids(anzahl):
+    #Boids werden erstellt und in der Liste gespeichert
     for i in range(anzahl):
         temp = Boid(randint(-30,30), randint(-30,30), randint(-30, 30), randint(0,360), randint(0,360), randint(0,360), uniform(0.0, 100.0), uniform(0.0, 10.0), 300.0, 1, groesse)
         Liste_Boids.append(temp)
@@ -264,11 +298,17 @@ def input(key):
     #https://www.ursinaengine.org/entity_basics.html 10.01.2022
     #10.01.2022
     if held_keys["+"]:
+        #Boid wird erstellt und der Liste hinzugefügt
         temp = Boid(randint(-30,30), randint(-30,30), randint(-30, 30), randint(0,360), randint(0,360), randint(0,360), uniform(50.0, 300.0), uniform(0.0, 10.0), 300.0, 1, groesse)
         Liste_Boids.append(temp)
+
     if held_keys["-"]:
+        #Der letzte Boid wird deaktiviert um ihn nicht mehr zu sehen
         Liste_Boids[len(Liste_Boids)-1].disable()
+        #letzter Boid wird aus der Liste entfernt
         Liste_Boids.pop()
+
+    #Der Modus jedes Boids wird geändert
     if held_keys["1"]:
         for i in Liste_Boids:
             i.setMode(1)
@@ -279,10 +319,12 @@ def input(key):
         for i in Liste_Boids:
             i.setMode(3)
 
-def create_instruction():
+def createInstruction():
     #https://www.youtube.com/watch?v=kb2wJYTwTHw 15.01.2022
+    #Anleitung wird erstellt
     text_Beschreibung = '''Kamera drehen:
                             \nRechtsklick + Mausbewegen
+                            \n\nZoom: Mausrad
                             \n\nKamerabewegen:
                             \nRechtsklick +
                             \nrechts:                      [A]
@@ -301,24 +343,30 @@ def create_instruction():
 
 
                         '''
+    #Textobjekt wird erstellt, skaliert und positioniert
     beschreibung = Text(text_Beschreibung, line_height = 0.5, scale = 0.7, x = -.8, y = -.12, color = color.white)
 
 #Anwendung 
 #https://www.ursinaengine.org/cheat_sheet.html#window 28.12.2021
+#Fenster wird erstellt
 app = Ursina()
 
 #Fenster
+#Fenstertitel wird festgelegt
 window.title = "Boids Simulation"
 
 #Kamera
 #https://www.ursinaengine.org/cheat_sheet.html#camera 30.12.2021
+#Kameraposition wird verändert um den Bereich von Anfang an zu sehen
 camera.position = (0,10,-350)
+#EditorCamera ermöglicht die Veränderung der Kamera
 EditorCamera()
 
 #Erstellen
 #Wireframe
-create_wireframe()
-create_instruction()
+createWireframe()
+#Anleitung
+createInstruction()
 #Boids
 ######################################################################################################################################
 anzahl = 25
@@ -326,6 +374,7 @@ groesse = 30
 ######################################################################################################################################
 
 Liste_Boids = []
-create_boids(anzahl)
+createBoids(anzahl)
 
+#Fenster wird ausgeführt
 app.run()
